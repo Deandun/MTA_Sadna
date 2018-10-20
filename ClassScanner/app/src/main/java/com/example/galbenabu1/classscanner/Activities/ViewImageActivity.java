3d9eaedef3c7bc84dbc9568169e668aab2b9a6e9
Chen Capon <Chen.Capon@niceactimize.com>
2018-10-20 10:38:11 +0300

    minor changes

--- ClassScanner/app/src/main/java/com/example/galbenabu1/classscanner/Activities/ViewImageActivity.java
+++ ClassScanner/app/src/main/java/com/example/galbenabu1/classscanner/Activities/ViewImageActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -21,11 +23,13 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import Logic.Database.DBManager;
 
 import Logic.Models.Album;
+import Logic.Services.CapturePhotoUtils;
 
 
 /**
@@ -60,6 +64,29 @@ public class ViewImageActivity extends AppCompatActivity {
 
         getImageByPathAndBitmap();
 
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setDrawingCacheEnabled(true);
+                Bitmap bitmap = imageView.getDrawingCache();
+//                File root = Environment.getExternalStorageDirectory();
+//                File file = new File(root.getAbsolutePath()+"/DCIM/Camera/img.jpg");
+                try
+                {
+//                    file.createNewFile();
+//                    FileOutputStream ostream = new FileOutputStream(file);
+//                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, ostream);
+                    MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "classImage" , "");
+                   // CapturePhotoUtils.insertImage(getContentResolver(), bitmap, "classImage" , "");
+                   // ostream.close();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,12 +119,7 @@ public class ViewImageActivity extends AppCompatActivity {
             }
         });
 
-        btnDownload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //
-            }
-        });
+
     }
 
 
