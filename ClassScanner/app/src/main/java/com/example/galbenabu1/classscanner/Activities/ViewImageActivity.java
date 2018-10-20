af22a8726176d1529d7bb71810d698197ba7f825
Deandun <rustykopo@gmail.com>
2018-10-20 17:34:40 +0300

    Removed unncesecerry class

--- ClassScanner/app/src/main/java/com/example/galbenabu1/classscanner/Activities/ViewImageActivity.java
+++ ClassScanner/app/src/main/java/com/example/galbenabu1/classscanner/Activities/ViewImageActivity.java
@@ -3,34 +3,22 @@ package com.example.galbenabu1.classscanner.Activities;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.widget.Toast;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.example.galbenabu1.classscanner.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 import Logic.Database.DBManager;
 
 import Logic.Models.Album;
-import Logic.Services.CapturePhotoUtils;
 
 
 /**
